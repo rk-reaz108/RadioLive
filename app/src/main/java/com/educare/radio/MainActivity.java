89e1e85b6d155127b0b833ac1dcccee469114c3b
Ibrar Tanim Rokan <ibrar.tanim@gmail.com>
2017-04-19 16:54:12 +0600

    Radio app first commit

---
+++ app/src/main/java/com/educare/radio/MainActivity.java
@@ -0,0 +1,290 @@
+
+
+package com.educare.radio;
+
+import android.app.Activity;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnBufferingUpdateListener;
+import android.media.MediaPlayer.OnPreparedListener;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+    private static final int RECORDER_SAMPLERATE = 8000;
+    private static final int RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_MONO;
+    private static final int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
+    private AudioRecord recorder = null;
+    private Thread recordingThread = null;
+    private boolean isRecording = false;
+
+    private ProgressBar playSeekBar;
+    private Button buttonPlay, btnStart, btnStop;
+    private Button buttonStopPlay;
+    private MediaPlayer player;
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initializeUIElements();
+        initializeMediaPlayer();
+
+
+        //setButtonHandlers();
+        //enableButtons(false);
+
+        int bufferSize = AudioRecord.getMinBufferSize(RECORDER_SAMPLERATE,
+                RECORDER_CHANNELS, RECORDER_AUDIO_ENCODING);
+
+        System.out.println("BUFFER SIZE VALUE IS " + bufferSize);
+    }
+
+    private void initializeUIElements() {
+
+        playSeekBar = (ProgressBar) findViewById(R.id.progressBar1);
+        playSeekBar.setMax(100);
+        playSeekBar.setVisibility(View.INVISIBLE);
+
+        btnStart = (Button) findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(this);
+
+        btnStop = (Button) findViewById(R.id.btnStop);
+        btnStop.setOnClickListener(this);
+
+        buttonPlay = (Button) findViewById(R.id.buttonPlay);
+        buttonPlay.setOnClickListener(this);
+
+
+        buttonStopPlay = (Button) findViewById(R.id.buttonStopPlay);
+        buttonStopPlay.setEnabled(false);
+        buttonStopPlay.setOnClickListener(this);
+
+    }
+
+
+
+    private void startPlaying() {
+        buttonStopPlay.setEnabled(true);
+        buttonPlay.setEnabled(false);
+
+        playSeekBar.setVisibility(View.VISIBLE);
+
+        player.prepareAsync();
+
+        player.setOnPreparedListener(new OnPreparedListener() {
+
+            public void onPrepared(MediaPlayer mp) {
+                player.start();
+            }
+        });
+
+    }
+
+    private void stopPlaying() {
+        if (player.isPlaying()) {
+            player.stop();
+            player.release();
+            initializeMediaPlayer();
+        }
+
+        buttonPlay.setEnabled(true);
+        buttonStopPlay.setEnabled(false);
+        playSeekBar.setVisibility(View.INVISIBLE);
+    }
+
+    private void initializeMediaPlayer() {
+        player = new MediaPlayer();
+        try {
+            //player.setDataSource("http://usa8-vn.mixstream.net:8138");
+            player.setDataSource("http://server2.crearradio.com:8371");
+
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        player.setOnBufferingUpdateListener(new OnBufferingUpdateListener() {
+
+            public void onBufferingUpdate(MediaPlayer mp, int percent) {
+                playSeekBar.setSecondaryProgress(percent);
+                Log.i("Buffering", "" + percent);
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (player.isPlaying()) {
+            player.stop();
+        }
+    }
+
+
+   /* private void setButtonHandlers() {
+        ((Button) findViewById(R.id.btnStart)).setOnClickListener(btnClick);
+        ((Button) findViewById(R.id.btnStop)).setOnClickListener(btnClick);
+    }*/
+
+   /* private void enableButton(int id, boolean isEnable) {
+        ((Button) findViewById(id)).setEnabled(isEnable);
+    }*/
+
+   /* private void enableButtons(boolean isRecording) {
+        enableButton(R.id.btnStart, !isRecording);
+        enableButton(R.id.btnStop, isRecording);
+    }*/
+
+    int BufferElements2Rec = 1024; // want to play 2048 (2K) since 2 bytes we
+    // use only 1024
+    int BytesPerElement = 2; // 2 bytes in 16bit format
+
+    private void startRecording() {
+
+        recorder = new AudioRecord(MediaRecorder.AudioSource.MIC,
+                RECORDER_SAMPLERATE, RECORDER_CHANNELS,
+                RECORDER_AUDIO_ENCODING, BufferElements2Rec * BytesPerElement);
+
+        recorder.startRecording();
+        isRecording = true;
+
+        recordingThread = new Thread(new Runnable() {
+
+            public void run() {
+
+                writeAudioDataToFile();
+
+            }
+        }, "AudioRecorder Thread");
+        recordingThread.start();
+    }
+
+    private byte[] short2byte(short[] sData) {
+        int shortArrsize = sData.length;
+        byte[] bytes = new byte[shortArrsize * 2];
+
+        for (int i = 0; i < shortArrsize; i++) {
+            bytes[i * 2] = (byte) (sData[i] & 0x00FF);
+            bytes[(i * 2) + 1] = (byte) (sData[i] >> 8);
+            sData[i] = 0;
+        }
+        return bytes;
+
+    }
+
+    private void writeAudioDataToFile() {
+        // Write the output audio in byte
+
+        String filePath = Environment.getExternalStorageDirectory()+"/edu_radio/voice8K16bitmono.wav";
+                ;
+        short sData[] = new short[BufferElements2Rec];
+
+        FileOutputStream os = null;
+        try {
+            os = new FileOutputStream(filePath);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        while (isRecording) {
+            // gets the voice output from microphone to byte format
+
+            recorder.read(sData, 0, BufferElements2Rec);
+            System.out.println("Short wirting to file" + sData.toString());
+            try {
+                // // writes the data to file from buffer
+                // // stores the voice buffer
+
+                byte bData[] = short2byte(sData);
+
+                os.write(bData, 0, BufferElements2Rec * BytesPerElement);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            os.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void stopRecording() {
+        // stops the recording activity
+        if (null != recorder) {
+            isRecording = false;
+
+            recorder.stop();
+            recorder.release();
+
+            recorder = null;
+            recordingThread = null;
+        }
+    }
+
+/*    private View.OnClickListener btnClick = new View.OnClickListener() {
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btnStart: {
+                    enableButtons(true);
+                    startRecording();
+                    break;
+                }
+                case R.id.btnStop: {
+                    enableButtons(false);
+                    stopRecording();
+                    break;
+                }
+            }
+        }
+    };*/
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+
+            finish();
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+    public void onClick(View v) {
+        if (v == buttonPlay) {
+            startPlaying();
+        } else if (v == buttonStopPlay) {
+            stopPlaying();
+        } else if (v == btnStart) {
+            //enableButtons(true);
+            startRecording();
+        } else if (v == btnStop) {
+            //enableButtons(false);
+            stopRecording();
+        }
+
+    }
+
+}
